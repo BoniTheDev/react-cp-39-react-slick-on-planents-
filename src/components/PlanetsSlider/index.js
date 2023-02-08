455ebd4349124cf7f5042d088e46966a559b5c32
BoniTheDev <boniramesh209@gmail.com>
2023-02-08 06:28:07 +0000

    reac-slick-for-using-carosuel

---
+++ src/components/PlanetsSlider/index.js
@@ -0,0 +1,32 @@
+// Write your code here
+import Slider from 'react-slick'
+
+import PlanetItem from '../PlanetItem'
+
+import './index.css'
+
+const PlanetsSlider = props => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    autoplay: true,
+    slidesToScroll: 1,
+    adaptiveHeight: true,
+  }
+  const {planetsList} = props
+
+  return (
+    <div className="planets-app-container" data-testid="planets">
+      <h1 className="heading">PLANETS</h1>
+      <Slider {...settings}>
+        {planetsList.map(eachPlanet => (
+          <PlanetItem key={eachPlanet.id} planetDetails={eachPlanet} />
+        ))}
+      </Slider>
+    </div>
+  )
+}
+
+export default PlanetsSlider
